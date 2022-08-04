45a5977676170eac0863d4ebd8d4671a92a51fb7
yusluv1234 <yusluv124@gmail.com>
2022-08-04 20:02:11 +0100

    Addiing Express functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
 mongoose.connect('mongodb://localhost/urlShortener', {
@@ -7,13 +8,17 @@ mongoose.connect('mongodb://localhost/urlShortener', {
 })
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const shorturls = await ShortUrl.find()
   res.render('index')
 })
 
-app.post('/shortUrls', (req, res) => {
+app.post('/shortUrls', async (req, res) => {
+ await ShortUrl.create({ full: req.body.fullUrl })
 
+ res.redirect('/')
 })
 
 app.listen(process.env.PORT || 5000)
