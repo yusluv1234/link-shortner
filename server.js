5e29510b73638a3d46dcb2c60a8e5b165ca57b13
yusluv1234 <yusluv124@gmail.com>
2022-08-04 21:50:33 +0100

    Done with this project

--- server.js
+++ server.js
@@ -11,14 +11,24 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
-  const shorturls = await ShortUrl.find()
-  res.render('index')
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
 })
 
 app.post('/shortUrls', async (req, res) => {
- await ShortUrl.create({ full: req.body.fullUrl })
+  await ShortUrl.create({ full: req.body.fullUrl })
 
- res.redirect('/')
+  res.redirect('/')
 })
 
-app.listen(process.env.PORT || 5000)
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 5000);
